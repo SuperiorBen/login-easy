bb3d79fb24df6f8de8b3e97a284ddb073d36b827
Héctor Benjamín Chipagua Calles <hector7chess@gmail.com>
2023-09-19 12:24:12 -0600

    Prueba socket

--- app/Http/Controllers/UserPropertyController.php
+++ app/Http/Controllers/UserPropertyController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Events\ShowProperties;
 
 class UserPropertyController extends Controller
 {
-    //
+    public function testProps(){
+        ShowProperties::dispatch('Hola sockets');
+    }
 }
--- resources/views/privada.blade.php
+++ resources/views/privada.blade.php
@@ -4,19 +4,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Privada</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a style="text-decoration: none; color:#fff;" class="navbar-brand btn btn-dark  float-end" 
-            href="{{ route('logout') }}">Logout</a>
+            <a style="text-decoration: none; color:#fff;" class="navbar-brand btn btn-dark  float-end" href="{{ route('logout') }}">Logout</a>
         </div>
     </nav>
 
-    <h1>Privada de @auth  {{ Auth::user()->name }}  @endauth</h1>
+    <h1>Privada de @auth {{ Auth::user()->name }} @endauth</h1>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+
+            console.log( window.Echo.private(`show-properties`)
+                .listen('ShowProperties'))
+
+            window.Echo.private(`show-properties`)
+                .listen('ShowProperties', (e) => {
+                    alert('hola')
+                    console.log(e);
+                });
+        }, false);
+    </script>
 </body>
 
 </html>
